dc3ab911fe54b374984625d8ffc6508cd4388c3c
Simran Preet Singh <simran5590@gmail.com>
2022-06-06 08:07:04 -0700

    Basics of Geometry reader

--- GeometryReader/ContentView.swift
+++ GeometryReader/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { proxy in
+            Image("picture")
+                .resizable()
+                .scaledToFit()
+                .frame(width: proxy.size.width)
+        }
+            
     }
 }
 
